c4bde308eb70be5f597dc2d78d23c3f4ec0c16fe
unknown <snorriv97@gmail.com>
2018-12-07 11:27:59 +0000

    repo update

--- Kóði/Repos/CarRepo.py
+++ Kóði/Repos/CarRepo.py
@@ -31,9 +31,9 @@ class CarRepo:
                 engine_size, horse_power, transmission, fuel_type, price, drive, total_km, 
                 tank_size, availability))
 
-    def get_car(self):
+    def get_cars(self):
         if self.__cars == []:
-            with open("./data/cars.csv", "r") as car_file:
+            with open("./Data/cars.csv", "r") as car_file:
                 for line in car_file.readlines():
                     (license_plate, make, model, manuf_year, car_class, seats, doors, color, weight, 
                         engine_size, horse_power, transmission, fuel_type, price, drive, total_km, 
@@ -43,3 +43,47 @@ class CarRepo:
                         total_km, tank_size, availability)
                     self.__cars.append(new_car)    
         return self.__cars
+
+    def all_cars(self):
+        return_list = []
+        all_cars = self.get_cars()
+        for car in all_cars:
+            return_list.append(car)
+        return return_list
+
+    def search_by_class(self, car_class):
+        return_list = []
+        all_cars = self.get_cars()
+        for car in all_cars:
+            if car[2] == car_class:
+                return_list.append(car)
+        return return_list
+
+    def search_by_model(self, model):
+        return_list = []
+        all_cars = self.get_cars()
+        for car in all_cars:
+            if car[3] == model:
+                return_list.append(car)
+        return return_list
+
+    def delete_car(self, license_plate):
+        all_cars = self.get_cars()
+        for index, car in enumerate(all_cars):
+            if car[0] == license_plate:
+                all_cars.pop(index)
+        with open("./Data/cars.csv", "w") as f:
+            for car in all_cars:
+                f.write(car)
+
+    def get_available_cars(self, availability):
+        return_list = []
+        all_cars = self.get_cars()
+        for car in all_cars:
+            if car[-1] == True:
+                return_list.append(car)
+        return return_list
+
+
+# + update_car_info()
+# + change_car_status()
